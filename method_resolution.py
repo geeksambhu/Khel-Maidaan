37081fb1910f6e3e9e04dd64e1ed7975f6873404
Shiva Gaire <geeksambhu@gmail.com>
2017-12-26 15:56:44 +0545

    Day 10: method resolution order in python multiple inheritance

--- method_resolution.py
+++ method_resolution.py
@@ -1,6 +1,21 @@
-#Hierarchy:
-    #               Animal Class
-    #              /     \
-    #            Pig   Spider  Nameable
-    #              \      |      /
-    #                 Spiderpig
+class A:
+    def whereiam(self):
+        print("I am in A")
+
+
+class B(A):
+    def whereiam(self):
+        print("I am in B")
+
+
+class C(A):
+    def whereiam(self):
+        print("I am in C")
+
+
+class D(B, C):
+    pass
+
+if __name__=="__main__":
+    d=D()
+    d.whereiam() #prints I am in B
